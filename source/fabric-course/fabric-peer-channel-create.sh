3e82b92c83952559a23e66f0e1de1ff4d66b7f2a
Joker.cheng <codepasser@outlook.com>
2020-03-27 15:57:02 +0800

    fabric-course: scripts

---
+++ source/fabric-course/fabric-peer-channel-create.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+export CHANNEL_NAME=fabric-course
+# the channel.tx file is mounted in the channel-artifacts directory within your CLI container
+# as a result, we pass the full path for the file
+# we also pass the path for the orderer ca-cert in order to verify the TLS handshake
+# be sure to export or replace the $CHANNEL_NAME variable appropriately
+peer channel create -o orderer.example.com:7050 -c $CHANNEL_NAME -f ./channel-artifacts/channel.tx --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
