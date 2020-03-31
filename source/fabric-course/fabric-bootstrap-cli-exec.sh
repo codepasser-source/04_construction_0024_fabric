e2070f8cee565c0bc476019bdbc18fffdf239e3d
Joker.cheng <codepasser@outlook.com>
2020-03-31 16:23:45 +0800

    fabric-course: fabric bootstrap cli exec

--- source/fabric-course/fabric-bootstrap-cli-exec.sh
+++ source/fabric-course/fabric-bootstrap-cli-exec.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-echo "#################################################################"
-echo "#######    Generating anchor peer update for Org1MSP   ##########"
-echo "#################################################################"
-
 # Channel create & join & update anchors
 docker exec cli ./scripts/cli-peer-channel.sh
 
