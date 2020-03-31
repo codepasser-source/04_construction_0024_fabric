3a67425e792fc8d80b31848e3a6531b0de2daab5
Joker.cheng <codepasser@outlook.com>
2020-03-31 14:07:45 +0800

    fabric-course: shell script tidy up

---
+++ source/fabric-course/scripts/cli-exec.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#!/bin/bash
+
+echo
+echo " ____    _____      _      ____    _____ "
+echo "/ ___|  |_   _|    / \    |  _ \  |_   _|"
+echo "\___ \    | |     / _ \   | |_) |   | |  "
+echo " ___) |   | |    / ___ \  |  _ <    | |  "
+echo "|____/    |_|   /_/   \_\ |_| \_\   |_|  "
+echo
+echo "Build your first network (BYFN) end-to-end test"
+echo
+export CHANNEL_NAME="$1"
+echo $CHANNEL_NAME
