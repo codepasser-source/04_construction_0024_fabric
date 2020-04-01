6541896354177bf92b0bdefafbe42e7cee3b5081
Joker.cheng <codepasser@outlook.com>
2020-04-01 16:59:41 +0800

    fabric 0003 course: kafka

---
+++ source/fabric-0003-course/fabric-docker-up.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo
+echo "##########################################################"
+echo "##### Docker container start up ##########################"
+echo "##########################################################"
+docker-compose -f docker-compose.yaml -f docker-compose-couch.yaml -f docker-compose-kafka.yaml up -d
