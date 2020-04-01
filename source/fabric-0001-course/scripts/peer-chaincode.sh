600e4eda5d4c4da99ae93d9d7af5c92022be010d
Joker.cheng <codepasser@outlook.com>
2020-04-01 14:04:21 +0800

    fabric 0001 course: baseline

--- source/fabric-0001-course/scripts/peer-chaincode.sh
+++ source/fabric-0001-course/scripts/peer-chaincode.sh
@@ -25,3 +25,28 @@ export CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric
 export CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp
 peer chaincode query -C $CHANNEL_NAME -n mycc -c '{"Args":["query","a"]}'
 peer chaincode query -C $CHANNEL_NAME -n mycc -c '{"Args":["query","b"]}'
+
+
+echo "#################################################################"
+echo "#######    Chaincode invoking peer1.org1.example.com:8051   #####"
+echo "#################################################################"
+export CORE_PEER_ADDRESS=peer1.org1.example.com:8051
+export CORE_PEER_LOCALMSPID=Org1MSP
+#export CORE_PEER_TLS_CERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/peers/peer1.org1.example.com/tls/server.crt
+#export CORE_PEER_TLS_KEY_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/peers/peer1.org1.example.com/tls/server.key
+export CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/peers/peer1.org1.example.com/tls/ca.crt
+export CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
+peer chaincode query -C $CHANNEL_NAME -n mycc -c '{"Args":["query","a"]}'
+peer chaincode query -C $CHANNEL_NAME -n mycc -c '{"Args":["query","b"]}'
+
+echo "#################################################################"
+echo "#######    Chaincode invoking peer1.org2.example.com:10051   #####"
+echo "#################################################################"
+export CORE_PEER_ADDRESS=peer1.org2.example.com:10051
+export CORE_PEER_LOCALMSPID=Org2MSP
+#export CORE_PEER_TLS_CERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/peers/peer1.org2.example.com/tls/server.crt
+#export CORE_PEER_TLS_KEY_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/peers/peer1.org2.example.com/tls/server.key
+export CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/peers/peer1.org2.example.com/tls/ca.crt
+export CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp
+peer chaincode query -C $CHANNEL_NAME -n mycc -c '{"Args":["query","a"]}'
+peer chaincode query -C $CHANNEL_NAME -n mycc -c '{"Args":["query","b"]}'
