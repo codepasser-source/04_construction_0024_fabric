385dc4e3f56a7b24fb59f827b4ec51b23337005c
Joker.cheng <codepasser@outlook.com>
2020-04-01 10:14:04 +0800

    fabric-course: 0001
    -  change chaincode language node

--- source/fabric-0001-course/scripts/cli-peer-chaincode.sh
+++ source/fabric-0001-course/scripts/cli-peer-chaincode.sh
@@ -22,11 +22,34 @@ export CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/pee
 echo_environment
 ## Golang
 # this installs the Go chaincode. For go chaincode -p takes the relative path from $GOPATH/src
-peer chaincode install -n mycc -v 1.0 -p github.com/chaincode/chaincode_example02/go/
+#peer chaincode install -n mycc -v 1.0 -p github.com/chaincode/chaincode_example02/go/
 ## Node
 ## make note of the -l node to indicate "node" chaincode
 ## for node chaincode -p takes the absolute path to the node.js chaincode
-#peer chaincode install -n mycc -v 1.0 -l node -p /opt/gopath/src/github.com/chaincode/chaincode_example02/node/
+peer chaincode install -n mycc -v 1.0 -l node -p /opt/gopath/src/github.com/chaincode/chaincode_example02/node/
+## Java
+## make note of the -l flag to indicate "java" chaincode
+## for java chaincode -p takes the absolute path to the java chaincode
+#peer chaincode install -n mycc -v 1.0 -l java -p /opt/gopath/src/github.com/chaincode/chaincode_example02/java/
+peer chaincode list --installed
+
+echo "#################################################################"
+echo "#######    Chaincode install peer1.org1.example.com:8051   ######"
+echo "#################################################################"
+export CORE_PEER_ADDRESS=peer1.org1.example.com:8051
+export CORE_PEER_LOCALMSPID=Org1MSP
+#export CORE_PEER_TLS_CERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/peers/peer1.org1.example.com/tls/server.crt
+#export CORE_PEER_TLS_KEY_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/peers/peer1.org1.example.com/tls/server.key
+export CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/peers/peer1.org1.example.com/tls/ca.crt
+export CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
+echo_environment
+## Golang
+# this installs the Go chaincode. For go chaincode -p takes the relative path from $GOPATH/src
+#peer chaincode install -n mycc -v 1.0 -p github.com/chaincode/chaincode_example02/go/
+## Node
+## make note of the -l node to indicate "node" chaincode
+## for node chaincode -p takes the absolute path to the node.js chaincode
+peer chaincode install -n mycc -v 1.0 -l node -p /opt/gopath/src/github.com/chaincode/chaincode_example02/node/
 ## Java
 ## make note of the -l flag to indicate "java" chaincode
 ## for java chaincode -p takes the absolute path to the java chaincode
@@ -45,11 +68,34 @@ export CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/pee
 echo_environment
 ## Golang
 # this installs the Go chaincode. For go chaincode -p takes the relative path from $GOPATH/src
-peer chaincode install -n mycc -v 1.0 -p github.com/chaincode/chaincode_example02/go/
+#peer chaincode install -n mycc -v 1.0 -p github.com/chaincode/chaincode_example02/go/
 ## Node
 ## make note of the -l node to indicate "node" chaincode
 ## for node chaincode -p takes the absolute path to the node.js chaincode
-#peer chaincode install -n mycc -v 1.0 -l node -p /opt/gopath/src/github.com/chaincode/chaincode_example02/node/
+peer chaincode install -n mycc -v 1.0 -l node -p /opt/gopath/src/github.com/chaincode/chaincode_example02/node/
+## Java
+## make note of the -l flag to indicate "java" chaincode
+## for java chaincode -p takes the absolute path to the java chaincode
+#peer chaincode install -n mycc -v 1.0 -l java -p /opt/gopath/src/github.com/chaincode/chaincode_example02/java/
+peer chaincode list --installed
+
+echo "#################################################################"
+echo "#######    Chaincode install peer1.org2.example.com:10051   #####"
+echo "#################################################################"
+export CORE_PEER_ADDRESS=peer1.org2.example.com:10051
+export CORE_PEER_LOCALMSPID=Org2MSP
+#export CORE_PEER_TLS_CERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/peers/peer1.org2.example.com/tls/server.crt
+#export CORE_PEER_TLS_KEY_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/peers/peer1.org2.example.com/tls/server.key
+export CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/peers/peer1.org2.example.com/tls/ca.crt
+export CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp
+echo_environment
+## Golang
+# this installs the Go chaincode. For go chaincode -p takes the relative path from $GOPATH/src
+#peer chaincode install -n mycc -v 1.0 -p github.com/chaincode/chaincode_example02/go/
+## Node
+## make note of the -l node to indicate "node" chaincode
+## for node chaincode -p takes the absolute path to the node.js chaincode
+peer chaincode install -n mycc -v 1.0 -l node -p /opt/gopath/src/github.com/chaincode/chaincode_example02/node/
 ## Java
 ## make note of the -l flag to indicate "java" chaincode
 ## for java chaincode -p takes the absolute path to the java chaincode
@@ -71,9 +117,9 @@ export CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric
 export CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
 echo_environment
 ## Golang
-peer chaincode instantiate -o orderer.example.com:7050 --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem -C $CHANNEL_NAME -n mycc -v 1.0 -c '{"Args":["init","a", "100", "b","200"]}' -P "AND ('Org1MSP.peer','Org2MSP.peer')"
+#peer chaincode instantiate -o orderer.example.com:7050 --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem -C $CHANNEL_NAME -n mycc -v 1.0 -c '{"Args":["init","a", "100", "b","200"]}' -P "AND ('Org1MSP.peer','Org2MSP.peer')"
 ## Node
-#peer chaincode instantiate -o orderer.example.com:7050 --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem -C $CHANNEL_NAME -n mycc -l node -v 1.0 -c '{"Args":["init","a", "100", "b","200"]}' -P "AND ('Org1MSP.peer','Org2MSP.peer')"
+peer chaincode instantiate -o orderer.example.com:7050 --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem -C $CHANNEL_NAME -n mycc -l node -v 1.0 -c '{"Args":["init","a", "100", "b","200"]}' -P "AND ('Org1MSP.peer','Org2MSP.peer')"
 ## Java
 #peer chaincode instantiate -o orderer.example.com:7050 --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem -C $CHANNEL_NAME -n mycc -l java -v 1.0 -c '{"Args":["init","a", "100", "b","200"]}' -P "AND ('Org1MSP.peer','Org2MSP.peer')"
 peer chaincode list --instantiated -C $CHANNEL_NAME
