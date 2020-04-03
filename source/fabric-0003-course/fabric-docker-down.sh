dedd1a724f1a777b234a2d1905db0af6b4a24624
Joker.cheng <codepasser@outlook.com>
2020-04-03 15:23:07 +0800

    fabric 0003 course: ca bootstrap

--- source/fabric-0003-course/fabric-docker-down.sh
+++ source/fabric-0003-course/fabric-docker-down.sh
@@ -3,4 +3,7 @@ echo
 echo "##########################################################"
 echo "##### Docker container shutdown ##########################"
 echo "##########################################################"
-docker-compose -f docker-compose.yaml -f docker-compose-couch.yaml -f docker-compose-kafka.yaml down
+# Exclude CA
+#docker-compose -f docker-compose.yaml -f docker-compose-couch.yaml -f docker-compose-kafka.yaml down
+# Include CA
+docker-compose -f docker-compose.yaml -f docker-compose-couch.yaml -f docker-compose-kafka.yaml -f docker-compose-ca.yaml down
--- source/fabric-0003-course/fabric-docker-up.sh
+++ source/fabric-0003-course/fabric-docker-up.sh
@@ -3,4 +3,11 @@ echo
 echo "##########################################################"
 echo "##### Docker container start up ##########################"
 echo "##########################################################"
-docker-compose -f docker-compose.yaml -f docker-compose-couch.yaml -f docker-compose-kafka.yaml up -d
+# Exclude CA
+#docker-compose -f docker-compose.yaml -f docker-compose-couch.yaml -f docker-compose-kafka.yaml up -d
+# Include CA
+export COURSE_CA1_PRIVATE_KEY=$(cd crypto-config/peerOrganizations/org1.example.com/ca && ls *_sk)
+export COURSE_CA2_PRIVATE_KEY=$(cd crypto-config/peerOrganizations/org2.example.com/ca && ls *_sk)
+echo "========== $COURSE_CA1_PRIVATE_KEY: "$COURSE_CA1_PRIVATE_KEY" =========="
+echo "========== $COURSE_CA2_PRIVATE_KEY: "$COURSE_CA2_PRIVATE_KEY" =========="
+docker-compose -f docker-compose.yaml -f docker-compose-couch.yaml -f docker-compose-kafka.yaml -f docker-compose-ca.yaml up -d
