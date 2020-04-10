8a73368214a7b57f78aa60389ceb4d518996ae56
Joker.cheng <codepasser@outlook.com>
2020-04-10 16:33:16 +0800

    fabric course: 0003
    org3

--- source/fabric-0003-course/org3/README.md
+++ source/fabric-0003-course/org3/README.md
@@ -48,14 +48,31 @@ configtxlator proto_decode --input config_block.pb --type common.Block | jq .dat
 jq -s '.[0] * {"channel_group":{"groups":{"Application":{"groups": {"Org3MSP":.[1]}}}}}' config.json ./channel-artifacts/org3.json > modified_config.json
 configtxlator proto_encode --input config.json --type common.Config --output config.pb
 configtxlator proto_encode --input modified_config.json --type common.Config --output modified_config.pb
-mv config_block.pb ./channel-artifacts/
-mv config.json ./channel-artifacts/
-mv modified_config.json ./channel-artifacts/
+configtxlator compute_update --channel_id $CHANNEL_NAME --original config.pb --updated modified_config.pb --output org3_update.pb
+configtxlator proto_decode --input org3_update.pb --type common.ConfigUpdate | jq . > org3_update.json
+echo '{"payload":{"header":{"channel_header":{"channel_id":"'$CHANNEL_NAME'", "type":2}},"data":{"config_update":'$(cat org3_update.json)'}}}' | jq . > org3_update_in_envelope.json
+configtxlator proto_encode --input org3_update_in_envelope.json --type common.Envelope --output org3_update_in_envelope.pb
 ```
 
 - 签名并提交配置更新
 
 ```shell script
 # ../script/cli-peer-org3.sh
+export CORE_PEER_ADDRESS=peer0.org1.example.com:7051
+export CORE_PEER_LOCALMSPID=Org1MSP
+export CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt
+export CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
 peer channel signconfigtx -f org3_update_in_envelope.pb
+
+export CORE_PEER_ADDRESS=peer0.org2.example.com:9051
+export CORE_PEER_LOCALMSPID="Org2MSP"
+export CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt
+export CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp
+peer channel signconfigtx -f org3_update_in_envelope.pb
+
+export CORE_PEER_LOCALMSPID="Org2MSP"
+export CORE_PEER_ADDRESS=peer0.org2.example.com:9051
+export CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt
+export CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp
+peer channel update -f org3_update_in_envelope.pb -c $CHANNEL_NAME -o orderer.example.com:7050 --tls --cafile $ORDERER_CA
 ```
--- source/fabric-0003-course/scripts/cli-peer-org3.sh
+++ source/fabric-0003-course/scripts/cli-peer-org3.sh
@@ -32,28 +32,29 @@ configtxlator proto_decode --input org3_update.pb --type common.ConfigUpdate | j
 echo '{"payload":{"header":{"channel_header":{"channel_id":"'$CHANNEL_NAME'", "type":2}},"data":{"config_update":'$(cat org3_update.json)'}}}' | jq . > org3_update_in_envelope.json
 configtxlator proto_encode --input org3_update_in_envelope.json --type common.Envelope --output org3_update_in_envelope.pb
 
-mv config_block.pb ./channel-artifacts/
-mv config.json ./channel-artifacts/
-mv modified_config.json ./channel-artifacts/
-mv config.pb ./channel-artifacts/
-mv modified_config.pb ./channel-artifacts/
-mv org3_update.pb ./channel-artifacts/
-mv org3_update.json ./channel-artifacts/
-mv org3_update_in_envelope.json ./channel-artifacts/
-mv org3_update_in_envelope.pb ./channel-artifacts/
+echo "#################################################################"
+echo "#######    Channel signconfigtx peer0.org1.example.com:7051 #####"
+echo "#################################################################"
+export CORE_PEER_ADDRESS=peer0.org1.example.com:7051
+export CORE_PEER_LOCALMSPID=Org1MSP
+export CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt
+export CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
+peer channel signconfigtx -f org3_update_in_envelope.pb
 
 echo "#################################################################"
 echo "#######    Channel signconfigtx peer0.org1.example.com:7051 #####"
 echo "#################################################################"
-peer channel signconfigtx -f org3_update_in_envelope.pb
-
-
-echo "#################################################################"
-echo "#######    Channel update peer0.org2.example.com:9051    ########"
-echo "#################################################################"
-export CORE_PEER_LOCALMSPID="Org2MSP"
 export CORE_PEER_ADDRESS=peer0.org2.example.com:9051
+export CORE_PEER_LOCALMSPID="Org2MSP"
 export CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt
 export CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp
-peer channel update -f org3_update_in_envelope.pb -c $CHANNEL_NAME -o orderer.example.com:7050 --tls --cafile $ORDERER_CA
+peer channel signconfigtx -f org3_update_in_envelope.pb
 
+echo "#################################################################"
+echo "#######    Channel update peer0.org1.example.com:7051    ########"
+echo "#################################################################"
+export CORE_PEER_ADDRESS=peer0.org1.example.com:7051
+export CORE_PEER_LOCALMSPID=Org1MSP
+export CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt
+export CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
+peer channel update -f org3_update_in_envelope.pb -c $CHANNEL_NAME -o orderer.example.com:7050 --tls --cafile $ORDERER_CA
