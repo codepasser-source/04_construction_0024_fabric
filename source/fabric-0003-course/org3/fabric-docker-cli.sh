1fdaf0a7f381348f9602d06f7cdac793a7e70a57
Joker.cheng <codepasser@outlook.com>
2020-04-10 17:27:41 +0800

    fabric course: 0003
    org3 join channel

--- source/fabric-0003-course/org3/fabric-docker-cli.sh
+++ source/fabric-0003-course/org3/fabric-docker-cli.sh
@@ -4,4 +4,3 @@ echo "##########################################################"
 echo "##### Docker container cli exec bash #####################"
 echo "##########################################################"
 docker exec -it cli3 bash
-docker exec -it Org3cli bash
