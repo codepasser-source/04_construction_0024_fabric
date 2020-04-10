136d5e8f6701aed68af13fdbfb73f5c3ade1abb5
Joker.cheng <codepasser@outlook.com>
2020-04-10 14:25:55 +0800

    fabric course: 0003
    org3

--- source/fabric-0003-course/org3/fabric-bootstrap-configtx-channel.sh
+++ source/fabric-0003-course/org3/fabric-bootstrap-configtx-channel.sh
@@ -4,4 +4,7 @@ echo "### Change channel configuration transaction 'channel.tx' org3 ##"
 echo "#################################################################"
 export FABRIC_CFG_PATH=$PWD && configtxgen -printOrg Org3MSP > ../channel-artifacts/org3.json
 
+echo "#################################################################"
+echo "### Copy channel crypto ordererOrganizations ####################"
+echo "#################################################################"
 cp -r ../crypto-config/ordererOrganizations ./crypto-config/
