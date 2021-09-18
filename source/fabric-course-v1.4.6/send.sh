fac3dea3aab8186a926e0e0dc7c1dec48b920cff
codepasser <yangyang.cheng@valueonline.cn>
2021-09-18 15:57:14 +0800

    fabric course v1.4.6: 0001
    bootstrap docker compose

--- source/fabric-course-v1.4.6/send.sh
+++ source/fabric-course-v1.4.6/send.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-rm -rf fabric.tar.gz
-tar cvf fabric.tar.gz 0001
-scp -P 220 fabric.tar.gz root@192.168.4.143:/data/src/github.com/hyperledger/fabric-course
+rm -rf fabric-course.tar.gz
+tar cvf fabric-course.tar.gz 0001
+scp -P 220 fabric-course.tar.gz root@192.168.4.143:/data/src/github.com/hyperledger/fabric-course
